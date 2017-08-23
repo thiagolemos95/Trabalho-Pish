bdc6a2da6f73391a4e04ec99de143641a748b053
BernardoM <bernardomonteiro77@hotmail.com>
2017-08-23 15:25:23 -0300

    Aplicativo pegando a imagem na nuvem e exibindo na tela

--- app/src/main/java/com/example/bernardo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bernardo/myapplication/MainActivity.java
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner spinner1;
     private Button btnSubmit;
+    private Spinner spinner3;
+    private Spinner spinner4;
+    private Button btnAgendar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,23 +57,41 @@ public class MainActivity extends AppCompatActivity {
     public void addListenerOnSpinnerItemSelection() {
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        spinner3.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        spinner4 = (Spinner) findViewById(R.id.spinner4);
+        spinner4.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
 
-    // get the selected dropdown list value
+    // Obter o valor do spinner selecionado
     public void addListenerOnButton() {
 
         spinner1 = (Spinner) findViewById(R.id.spinner1);
-
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        spinner4 = (Spinner) findViewById(R.id.spinner4);
         btnSubmit = (Button) findViewById(R.id.button);
-
+        btnAgendar = (Button) findViewById(R.id.button2);
         btnSubmit.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
                 Toast.makeText(MainActivity.this,
-                        "OnClickListener : " +
-                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()),
+                        "Abastecido com \n "
+                                + String.valueOf(spinner1.getSelectedItem()) + " de ração",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        btnAgendar.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,
+                        "Agendado para  : " +
+                                "\n  "+ String.valueOf(spinner3.getSelectedItem())
+                        + " e " + String.valueOf(spinner4.getSelectedItem()),
                         Toast.LENGTH_SHORT).show();
             }
 
